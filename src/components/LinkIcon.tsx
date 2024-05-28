16bc9b237bc20653507597f94786f3adb9e94079
George Butler <georgebutleriv@gmail.com>
2024-05-27 23:49:06 -0700

    prettier :)

--- src/components/LinkIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function LinkIcon({ size = '1rem', children }: { size?: string, children: React.ReactNode }) {
-    return (
-        <a href="https://github.com" target="_blank" className="inline-block">
-            {children}
-        </a>
-    );
-}
