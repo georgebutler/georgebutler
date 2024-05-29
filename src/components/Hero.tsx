0183b8b556d16c1aa196a847d9df234c5328723c
George Butler <georgebutleriv@gmail.com>
2024-05-28 22:58:27 -0700

    the vercel look

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import { Canvas } from "@react-three/fiber";
 
 export default function Hero() {
     return (
-        <Canvas>
+        <Canvas resize={{ debounce: 0 }} style={{ position: 'absolute' }} className="h-full -z-10">
             <ambientLight intensity={0.1} />
             <directionalLight color="red" position={[0, 0, 5]} />
             <mesh>
