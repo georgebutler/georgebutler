0acd6830883c738c434419cbc3609e175a17b2c5
George Butler <georgebutleriv@gmail.com>
2024-06-04 09:50:08 -0700

    Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(), tailwind()]
+  integrations: [react(), mdx(), tailwind()],
+  output: "server",
+  adapter: netlify()
 });
