058ddd762fa9d262f3b4fba0a54884e31816939e
George Butler <georgebutleriv@gmail.com>
2024-06-15 13:43:01 -0700

    styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
+import { motion } from 'framer-motion';
+
+const NavbarLink = ({ href, target, children }) => {
+    return (
+        <motion.a
+            href={href}
+            target={target}
+            className="text-sm"
+            whileHover={{ color: "#3498db" }}
+        >
+            {children}
+        </motion.a>
+    );
+};
+
 const Navbar = () => {
     return (
         <nav>
-            <div class="container">
-                <a href="/">Home</a>
+            <div className="container mt-4">
+                <div className="flex flex-row gap-4 font-bold tracking-tighter">
+                    <NavbarLink href="/" target="">Home</NavbarLink>
+                    <NavbarLink href="/blog" target="">Blog</NavbarLink>
+                    <NavbarLink href="/experiments" target="">Experiments</NavbarLink>
+                </div>
             </div>
         </nav>
     );
--- src/components/VideoHero.jsx
+++ src/components/VideoHero.jsx
@@ -1,116 +1,47 @@
-import React, { useEffect, useRef } from 'react';
-import { Canvas, extend, useThree, useFrame } from '@react-three/fiber';
-import { useVideoTexture, shaderMaterial, OrbitControls } from '@react-three/drei';
-import * as THREE from 'three';
+import * as THREE from 'three'
+import React, { Suspense, useEffect, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Reflector, Text, useTexture, useGLTF, Point } from '@react-three/drei'
 
-const vertexShader = `
-uniform sampler2D map;
-
-uniform float width;
-uniform float height;
-uniform float nearClipping, farClipping;
-
-uniform float pointSize;
-uniform float zOffset;
-
-varying vec2 vUv;
-
-const float XtoZ = 1.11146; // tan( 1.0144686 / 2.0 ) * 2.0;
-const float YtoZ = 0.83359; // tan( 0.7898090 / 2.0 ) * 2.0;
-
-void main() {
-
-  vUv = vec2( position.x / width, position.y / height );
-
-  vec4 color = texture2D( map, vUv );
-  float depth = ( color.r + color.g + color.b ) / 3.0;
-
-  // Projection code by @kcmic
-
-  float z = ( 1.0 - depth ) * (farClipping - nearClipping) + nearClipping;
-
-  vec4 pos = vec4(
-    ( position.x / width - 0.5 ) * z * XtoZ,
-    ( position.y / height - 0.5 ) * z * YtoZ,
-    - z + zOffset,
-    1.0);
-
-  gl_PointSize = pointSize;
-  gl_Position = projectionMatrix * modelViewMatrix * pos;
-
+function Carla(props) {
+  const { scene } = useGLTF('/assets/carla-draco.glb')
+  return <primitive object={scene} {...props} />
 }
-`;
 
-const fragmentShader = `
-uniform sampler2D map;
-
-varying vec2 vUv;
-
-void main() {
-
-  vec4 color = texture2D( map, vUv );
-  gl_FragColor = vec4( color.r, color.g, color.b, 0.2 );
-
-}
-`;
-
-const VideoShaderMaterial = shaderMaterial(
-  {
-    map: null,
-    width: 640,
-    height: 480,
-    nearClipping: 1,
-    farClipping: 3000,
-    pointSize: 2,
-    zOffset: 1500,
-  },
-  vertexShader,
-  fragmentShader
-);
-
-extend({ VideoShaderMaterial });
-
-const PointCloud = () => {
-  const meshRef = useRef();
-  const videoTexture = useVideoTexture('/textures/kinect.mp4', { start: true, muted: true });
-
-  useEffect(() => {
-    const width = 640;
-    const height = 480;
-
-    const vertices = new Float32Array(width * height * 3);
-    for (let i = 0, j = 0, l = vertices.length; i < l; i += 3, j++) {
-      vertices[i] = j % width;
-      vertices[i + 1] = Math.floor(j / width);
-    }
-
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-
-    if (meshRef.current) {
-      meshRef.current.geometry = geometry;
-      meshRef.current.material.uniforms.map.value = videoTexture;
-    }
-  }, [videoTexture]);
-
+function VideoText(props) {
+  const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/assets/drei.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }))
+  useEffect(() => void video.play(), [video])
   return (
-    <points ref={meshRef}>
-      <bufferGeometry />
-      <videoShaderMaterial
-        attach="material"
-        transparent
-        blending={THREE.AdditiveBlending}
-        depthTest={false}
-        depthWrite={false}
-      />
-    </points>
-  );
-};
+    <Text font="/assets/Inter-Bold.woff" fontSize={3} letterSpacing={-0.06} {...props}>
+      George Butler
+      <meshBasicMaterial toneMapped={false}>
+        <videoTexture attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
+      </meshBasicMaterial>
+    </Text>
+  )
+}
+
+function Ground() {
+  const [floor, normal] = useTexture(['/assets/SurfaceImperfections003_1K_var1.jpg', '/assets/SurfaceImperfections003_1K_Normal.jpg'])
+  return (
+    <Reflector blur={[400, 100]} resolution={512} args={[10, 10]} mirror={0.5} mixBlur={6} mixStrength={1.5} rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
+      {(Material, props) => <Material color="#a0a0a0" metalness={0.4} roughnessMap={floor} normalMap={normal} normalScale={[2, 2]} {...props} />}
+    </Reflector>
+  )
+}
 
 const VideoHero = () => {
   return (
-    <Canvas style={{ position: 'absolute' }} className="h-full -z-10" camera={{ position: [0, 0, 1200], fov: 50, far: 3000 }}>
-      <PointCloud />
+    <Canvas style={{ position: 'absolute' }} className="h-full -z-10" gl={{ alpha: false }} camera={{ position: [0, 1, 4], fov: 45 }}>
+      <color attach="background" args={['black']} />
+      <fog attach="fog" args={['black', 15, 20]} />
+      <Points
+  limit={1000} // Optional: max amount of items (for calculating buffer size)
+  range={1000} // Optional: draw-range
+>
+  <pointsMaterial vertexColors />
+  <Point position={[1, 2, 3]} color="red" />
+</Points>
     </Canvas>
   );
 };
