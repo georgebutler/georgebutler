80f19b1165053da50407e52851b112f88ded0806
George Butler <georgebutleriv@gmail.com>
2024-06-21 11:47:33 -0700

    Test

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,12 +1,15 @@
 const base = "prose";
 const heading = "prose-headings:text-white prose-h1:text-4xl prose-h1:font-bold";
-const paragraphClasses = "prose-p:text-justify prose-p:text-white";
-const linkClasses = "prose-a:font-bold prose-a:text-[#3498db] prose-a:no-underline hover:prose-a:underline";
-const strongClasses = "prose-strong:text-white";
-const imageClasses = "prose-img:rounded-xl";
+const paragraph = "prose-p:text-justify prose-p:text-white";
+const link = "prose-a:font-bold prose-a:text-[#3498db] prose-a:no-underline hover:prose-a:underline";
+const strong = "prose-strong:text-white";
+const image = "prose-img:rounded-xl";
+const list = "prose-li:text-white";
+const code = "prose-code:text-white";
+const td = "prose-td:text-white";
 
 const Article = ({ children }) => (
-  <article className={`${base} ${heading} ${paragraphClasses} ${linkClasses} ${strongClasses} ${imageClasses}`}>
+  <article className={`${base} ${heading} ${paragraph} ${link} ${strong} ${image} ${list} ${code} ${td}`}>
     {children}
   </article>
 );
