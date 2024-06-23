849e7fd35afdc66af6fc8a60ec894b60f3587ac6
George Butler <georgebutleriv@gmail.com>
2024-06-22 17:51:53 -0700

    Generic post

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), mdx()],
   output: "server",
   adapter: netlify()
 });
