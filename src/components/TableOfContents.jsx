b43901977773dc8751c501f7f2018b41ea40e3f4
George Butler <georgebutleriv@gmail.com>
2024-06-24 15:48:32 -0700

    Table of contents :)

--- src/components/TableOfContents.jsx
+++ src/components/TableOfContents.jsx
@@ -1,10 +1,15 @@
 const TableOfContents = ({ headings }) => {
     return (
         <nav>
-            <ul className="text-nowrap text-sm p-4 list-decimal list-inside rounded bg-[#0A0A0A]">
+            <ul className="text-nowrap px-4 py-4 list-none rounded bg-[#0A0A0A]">
                 {headings.map((heading) => (
-                    <li>
-                        <a className="text-[#3498db] hover:underline font-bold" href={`#${heading.slug}`}>{heading.text}</a>
+                    <li key={heading.slug} className={heading.depth > 1 ? `px-${heading.depth} text-sm` : ''}>
+                        <a
+                            className="text-[#3498db] hover:underline font-bold"
+                            href={`#${heading.slug}`}
+                        >
+                            {heading.text}
+                        </a>
                     </li>
                 ))}
             </ul>
