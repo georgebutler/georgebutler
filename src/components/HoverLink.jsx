44cea5b602c95752c6b6c0f63c6d372e576f4942
georgebutler <georgebutleriv@gmail.com>
2024-06-28 16:01:38 -0700

    Update hover link

--- src/components/HoverLink.jsx
+++ src/components/HoverLink.jsx
@@ -1,12 +1,32 @@
 import { motion } from 'framer-motion';
 
-const HoverLink = ({ href, target, children }) => {
+const HoverLink = ({ href, target = "", index = 0, children }) => {
     return (
         <motion.a
             href={href}
             target={target}
             className="text-sm"
-            whileHover={{ x: 5, color: "#3498db" }}
+            whileHover={{
+                x: 5,
+                color: "#3498db",
+                delay: 0
+            }}
+            initial={{
+                opacity: 0,
+                y: 14
+            }}
+            animate={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                    delay: index * 0.1,
+                    duration: 0.1,
+                    type: "spring",
+                    stiffness: 100,
+                    damping: 20,
+                }
+            }}
+            key={href}
         >
             {children}
         </motion.a>
